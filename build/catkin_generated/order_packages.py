a39782840d62c6365b29f4425b84b4f61de2f0a9
ryan <ryanli588@ucsb.edu>
2024-01-19 04:01:46 -0800

    ryan

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hero1xxx/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hero1xxx/autosim_ws/devel;/opt/ros/melodic'.split(';') if '/home/hero1xxx/autosim_ws/devel;/opt/ros/melodic' != '' else []
---
+++ build/f1tenth_simulator/CMakeFiles/wall_follow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wall_follow.dir/node/wall_follow.cpp.o"
+  "/home/hero1xxx/catkin_ws/devel/lib/f1tenth_simulator/wall_follow.pdb"
+  "/home/hero1xxx/catkin_ws/devel/lib/f1tenth_simulator/wall_follow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wall_follow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
