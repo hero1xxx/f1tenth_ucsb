76182ab779734c25be5d008609ec5610e7b05543
Ryan Li <92003880+hero1xxx@users.noreply.github.com>
2024-01-26 10:16:10 -0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hero1xxx/catkin_ws/src
-# Build directory: /home/hero1xxx/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("beginner_tutorials")
-subdirs("f1tenth_simulator")
--- build/f1tenth_simulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hero1xxx/catkin_ws/src/f1tenth_simulator
-# Build directory: /home/hero1xxx/catkin_ws/build/f1tenth_simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
